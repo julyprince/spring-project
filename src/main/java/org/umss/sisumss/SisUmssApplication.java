816cfffb2e1bd736915b540025d6f038919a3784
julyprince <princejec8080@gmail.com>
2022-09-21 12:21:18 -0400

    initial commit

---
+++ src/main/java/org/umss/sisumss/SisUmssApplication.java
@@ -0,0 +1,13 @@
+package org.umss.sisumss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisUmssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisUmssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/umss/sisumss/SisUmssApplicationTests.java
@@ -0,0 +1,13 @@
+package org.umss.sisumss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisUmssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
